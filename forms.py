c5834f37323d22d3c56709b1cd10a5a35155d4d3
Michael Lichtenberger <mikelichtenberger81@gmail.com>
2024-05-28 13:21:06 -0400

    base login signup htmls

---
+++ forms.py
@@ -0,0 +1,22 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, TextAreaField
+from wtforms.validators import DataRequired, Email, Length
+
+class UserAddForm(FlaskForm):
+    '''Form for adding users'''
+
+    username = StringField('Username', validators=[DataRequired()])
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[Length(min=8)])
+
+class LoginForm(FlaskForm):
+    '''Form for logging in user'''
+
+    username = StringField('Username', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[Length(min=8)])
+
+class CocktailForm(FlaskForm):
+    '''Form for choosing cocktail to make'''
+
+    cocktail = StringField('Cocktail')
+    ingredients = StringField('Ingredient')
